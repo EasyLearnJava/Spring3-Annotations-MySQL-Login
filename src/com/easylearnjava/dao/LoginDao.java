22873a6faab0eb21a07b959e0dad4c7e5b0d206f
raghu <easylearnjava4u@gmail.com>
2014-05-24 16:01:30 -0500

    Spring3 Login functionality

---
+++ src/com/easylearnjava/dao/LoginDao.java
@@ -0,0 +1,41 @@
+package com.easylearnjava.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.springframework.stereotype.Repository;
+
+import com.easylearnjava.exception.ServiceException;
+import com.easylearnjava.util.DBConnection;
+
+@Repository
+public class LoginDao {
+
+	public String getUserPassword(String userName) {
+
+		Connection con;
+		PreparedStatement stmt;
+		DBConnection dbConn = new DBConnection();
+		String passwordFromDB = null;
+
+		try {
+			con = dbConn.getDBConnection();
+
+			String sql = "SELECT user_password FROM  USER where user_name = ? ";
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, userName);
+
+			ResultSet res = stmt.executeQuery();
+			while (res.next()) {
+				passwordFromDB = res.getString("USER_PASSWORD");
+			}
+			dbConn.closeConnection();
+		} catch (Exception ex) {
+			ex.printStackTrace(); 
+			throw new ServiceException("global.exception.message");
+		}
+		return passwordFromDB;
+	}
+
+}
